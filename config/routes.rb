fcfc7edbcb520c22dfd76175da84c1a5d63861f4
Amine_Zerradi <dasileker@gmail.com>
2021-08-07 13:29:25 +0100

    add initial files

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :appointments
+      end
+
+      post '/login', to: 'users#login'
+      get '/auto_login', to: 'users#auto_login'
+
+      resources :therapists do
+        resources :appointments
+      end
+    end
+  end
+end
